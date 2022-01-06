9f433cee7b868a6a183525d59f56c5503360051a
anuragshukla07 <anuragshukla2k12@gmail.com>
2022-01-06 21:04:31 +0530

    'initial-commit'

---
+++ app.js
@@ -0,0 +1,14 @@
+document.addEventListener('DOMContentLoaded', ()=>{
+const grid = document.querySelector('.grid')
+let squares = Array.from(document.querySelectorAll('.grid div'))
+const Scoredisplay = document.querySelector('#score')
+const StartBtn = document.querySelector('#start-button')
+const width = 10;
+
+console.log(squares)
+
+
+}
+)
+
+
